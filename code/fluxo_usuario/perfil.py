bf77db6f3b4e70563c36263cd9cc170c0c7d7f04
Manoel Messias <manoelmpaz@gmail.com>
2026-06-12 18:36:12 -0300

    Ajustes finais

--- code/fluxo_usuario/perfil.py
+++ code/fluxo_usuario/perfil.py
@@ -36,7 +36,7 @@ def atualizar_perfil():
         if not novo:
             break
         if dados.dados.validar_e_calcular_idade(novo):
-            u['idade'] = novo
+            u['idade'] = dados.dados.validar_e_calcular_idade(novo)
             break
         print("  Idade inválida, tente novamente...")
 
